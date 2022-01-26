c19f0d4ed9dff5bd1b187ae785cbbed91b6f5004
eduardVarnavskiy <eduardvarnavskiy@yahoo.es>
2022-01-26 16:21:46 +0100

    Relaciones,ventas

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -48,6 +48,10 @@ public class CacheConfiguration {
             createCache(cm, com.mycompany.myapp.domain.User.class.getName());
             createCache(cm, com.mycompany.myapp.domain.Authority.class.getName());
             createCache(cm, com.mycompany.myapp.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.mycompany.myapp.domain.Cliente.class.getName());
+            createCache(cm, com.mycompany.myapp.domain.Venta.class.getName());
+            createCache(cm, com.mycompany.myapp.domain.Empleado.class.getName());
+            createCache(cm, com.mycompany.myapp.domain.Coche.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,26 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'cliente',
+        data: { pageTitle: 'cocesionarioSimApp.cliente.home.title' },
+        loadChildren: () => import('./cliente/cliente.module').then(m => m.ClienteModule),
+      },
+      {
+        path: 'venta',
+        data: { pageTitle: 'cocesionarioSimApp.venta.home.title' },
+        loadChildren: () => import('./venta/venta.module').then(m => m.VentaModule),
+      },
+      {
+        path: 'empleado',
+        data: { pageTitle: 'cocesionarioSimApp.empleado.home.title' },
+        loadChildren: () => import('./empleado/empleado.module').then(m => m.EmpleadoModule),
+      },
+      {
+        path: 'coche',
+        data: { pageTitle: 'cocesionarioSimApp.coche.home.title' },
+        loadChildren: () => import('./coche/coche.module').then(m => m.CocheModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
